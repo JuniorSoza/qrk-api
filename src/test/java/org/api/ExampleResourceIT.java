ccce7a8070e0e57ec068b1ca0efccae365a15946
wanchundia <wanchundia@lafabril.com.ec>
2023-01-23 09:40:39 -0500

    test

--- src/test/java/org/api/ExampleResourceIT.java
+++ src/test/java/org/api/ExampleResourceIT.java
@@ -1,8 +1,8 @@
-package org.acme;
+package org.api;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class ExampleResourceIT extends ExampleResourceTest {
+public class ExampleResourceIT extends org.api.ExampleResourceTest {
     // Execute the same tests but in packaged mode.
 }
